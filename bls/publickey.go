e8bd0da69c89fadb15c1d92a03274c53574aa798
Julian Meyer <julianmeyer2000@gmail.com>
2020-05-26 12:49:31 -0700

    Split bls interface

---
+++ bls/publickey.go
@@ -0,0 +1,77 @@
+package bls
+
+import (
+	"fmt"
+
+	"github.com/olympus-protocol/bls-go/bls"
+	"github.com/olympus-protocol/ogen/utils/bech32"
+	"github.com/olympus-protocol/ogen/utils/chainhash"
+	"github.com/pkg/errors"
+)
+
+// PublicKey used in the BLS signature scheme.
+type PublicKey struct {
+	p *bls.PublicKey
+}
+
+// ToWIF converts the private key to a Bech32 encoded string.
+func (p *PublicKey) ToWIF(privPrefix string) (string, error) {
+	return bech32.Encode(privPrefix, p.Marshal()), nil
+}
+
+// PublicKeyFromBytes creates a BLS public key from a  BigEndian byte slice.
+func PublicKeyFromBytes(pub []byte) (*PublicKey, error) {
+	if len(pub) != 48 {
+		return nil, fmt.Errorf("public key must be %d bytes", 48)
+	}
+	if cv, ok := pubkeyCache.Get(string(pub)); ok {
+		return cv.(*PublicKey).Copy()
+	}
+	pubKey := &bls.PublicKey{}
+	err := pubKey.Deserialize(pub)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not unmarshal bytes into public key")
+	}
+	pubKeyObj := &PublicKey{p: pubKey}
+	copiedKey, err := pubKeyObj.Copy()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not copy public key")
+	}
+	pubkeyCache.Set(string(pub), copiedKey, 48)
+	return pubKeyObj, nil
+}
+
+// Marshal a public key into a LittleEndian byte slice.
+func (p *PublicKey) Marshal() []byte {
+	return p.p.Serialize()
+}
+
+// Copy the public key to a new pointer reference.
+func (p *PublicKey) Copy() (*PublicKey, error) {
+	np := *p.p
+	return &PublicKey{p: &np}, nil
+}
+
+// Aggregate two public keys.
+func (p *PublicKey) Aggregate(p2 *PublicKey) *PublicKey {
+	p.p.Add(p2.p)
+	return p
+}
+
+// ToAddress converts the public key to a Bech32 address.
+func (p *PublicKey) ToAddress(pubPrefix string) (string, error) {
+	out := make([]byte, 20)
+	pkS := p.p.Serialize()
+	h := chainhash.HashH(pkS[:])
+	copy(out[:], h[:20])
+	return bech32.Encode(pubPrefix, out), nil
+}
+
+// Hash calculates the hash of the public key.
+func (p *PublicKey) Hash() [20]byte {
+	pkS := p.p.Serialize()
+	h := chainhash.HashH(pkS[:])
+	var hBytes [20]byte
+	copy(hBytes[:], h[:])
+	return hBytes
+}
---
+++ bls/secretkey.go
@@ -0,0 +1,70 @@
+package bls
+
+import (
+	"fmt"
+
+	"github.com/olympus-protocol/bls-go/bls"
+	"github.com/pkg/errors"
+)
+
+// SecretKey used in the BLS signature scheme.
+type SecretKey struct {
+	p *bls.SecretKey
+}
+
+// RandKey creates a new private key using a random method provided as an io.Reader.
+func RandKey() *SecretKey {
+	secKey := &bls.SecretKey{}
+	secKey.SetByCSPRNG()
+	return &SecretKey{secKey}
+}
+
+func DeriveSecretKey(bs []byte) *SecretKey {
+	return &SecretKey{bls.DeriveSecretKey(bs)}
+}
+
+// SecretKeyFromBytes creates a BLS private key from a BigEndian byte slice.
+func SecretKeyFromBytes(priv []byte) (*SecretKey, error) {
+	if len(priv) != 32 {
+		return nil, fmt.Errorf("secret key must be %d bytes", 32)
+	}
+	secKey := &bls.SecretKey{}
+	err := secKey.Deserialize(priv)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not unmarshal bytes into secret key")
+	}
+	return &SecretKey{p: secKey}, err
+}
+
+// PublicKey obtains the public key corresponding to the BLS secret key.
+func (s *SecretKey) PublicKey() *PublicKey {
+	return &PublicKey{p: s.p.GetPublicKey()}
+}
+
+// Sign a message using a secret key - in a beacon/validator client.
+//
+// In IETF draft BLS specification:
+// Sign(SK, message) -> signature: a signing algorithm that generates
+//      a deterministic signature given a secret key SK and a message.
+//
+// In ETH2.0 specification:
+// def Sign(SK: int, message: Bytes) -> BLSSignature
+func (s *SecretKey) Sign(msg []byte) *Signature {
+	signature := s.p.SignByte(msg)
+	return &Signature{s: signature}
+}
+
+// Marshal a secret key into a LittleEndian byte slice.
+func (s *SecretKey) Marshal() []byte {
+	keyBytes := s.p.Serialize()
+	if len(keyBytes) < 32 {
+		emptyBytes := make([]byte, 32-len(keyBytes))
+		keyBytes = append(emptyBytes, keyBytes...)
+	}
+	return keyBytes
+}
+
+// Add adds two secret keys together.
+func (s *SecretKey) Add(other *SecretKey) {
+	s.p.Add(other.p)
+}
