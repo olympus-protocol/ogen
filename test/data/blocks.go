22911a9e811b04f616ab6b2dfccaca18aef68ba8
Enrique <eabz@polispay.org>
2020-06-26 11:18:45 -0500

    Move db (#84)

--- test/data/blocks.go
+++ test/data/blocks.go
@@ -3,9 +3,20 @@ package testdata
 import (
 	"time"
 
+	"github.com/olympus-protocol/ogen/bdb"
 	"github.com/olympus-protocol/ogen/bls"
 	"github.com/olympus-protocol/ogen/primitives"
+	"github.com/olympus-protocol/ogen/utils/chainhash"
 )
+
+var BlockRow = bdb.BlockNodeDisk{
+	StateRoot: *Hash,
+	Height:    1000,
+	Slot:      1000,
+	Children:  make([]chainhash.Hash, 32),
+	Hash:      *Hash,
+	Parent:    *Hash,
+}
 
 var BlockHeader = primitives.BlockHeader{
 	Version:                    1,
