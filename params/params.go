2207b5131bc19134e3b626d43a8830b802f6ef67
Enrique Berrueta <eabz@polispay.org>
2020-07-02 15:39:21 -0500

    Update params.go

--- params/params.go
+++ params/params.go
@@ -95,7 +95,7 @@ var Mainnet = ChainParams{
 	},
 }
 
-var testnetChainFileHash, _ = chainhash.NewHashFromStr("780eb5f8e710cb37d6e2020f6b16497d67f7c5d3c2300bb6f451aaa3315b778f")
+var testnetChainFileHash, _ = chainhash.NewHashFromStr("23288d2683570d57dbbd101ae10a700abaaed3b794fea6e2274f17bd536af898")
 
 // TestNet are chain parameters used for the testnet.
 var TestNet = ChainParams{
