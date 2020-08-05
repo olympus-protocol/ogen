a9d4b475700b9df5f9181edbf09296c20dbb9981
Aether-eum <aether@polispay.org>
2020-08-05 00:26:38 -0400

    cli unit testing and support for single-line commands

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,1 +1,198 @@
 package cli_test
+
+import (
+	"github.com/olympus-protocol/ogen/cli/rpcclient"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func NewRootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "cli_test",
+		Short: "Mock RPC Cli",
+		Long:  `Simulates the functionality of the cli (single command only)`,
+		Run:   rpcclient.Run,
+	}
+}
+
+func Test_ChainCommands(t *testing.T) {
+	cmd := NewRootCmd()
+
+	cmd.SetArgs([]string{"getchaininfo"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getrawblock <hash>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getblockhash <height>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getblock <hash>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getaccountinfo <account>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"gettransaction <txid>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+}
+
+func Test_ValidatorCommands(t *testing.T) {
+	cmd := NewRootCmd()
+
+	cmd.SetArgs([]string{"getvalidatorslist"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getaccountvalidators <account>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+
+}
+
+func Test_NetworkCommands(t *testing.T) {
+	cmd := NewRootCmd()
+
+	cmd.SetArgs([]string{"getnetworkinfo"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getpeersinfo <account>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"addpeer <addr>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+}
+
+func Test_UtilCommands(t *testing.T) {
+	cmd := NewRootCmd()
+
+	cmd.SetArgs([]string{"startproposer <keystore_pass>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"stopproposer"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"submitrawdata <raw_data> <type>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"genkeypair"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"genrawkeypair"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"genvalidatorkey <keys> <pass>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"decoderawtransaction <raw_tx>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"decoderawblock <raw_block>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+}
+
+func Test_WalletCommands(t *testing.T) {
+	cmd := NewRootCmd()
+
+	cmd.SetArgs([]string{"listwallets"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"createwallet <name> <pass>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"openwallet <name> <pass>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"closewallet"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"importwallet <name> <wif>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"dumpwallet"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getbalance"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getvalidators"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"getaccount"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"sendtransaction <account> <amount>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"startvalidator <privkey>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+	cmd.SetArgs([]string{"exitvalidator <pubkey>"})
+	assert.NotPanics(t, func() {
+		err := cmd.Execute()
+		assert.Nil(t, err)
+	})
+}
--- cli/rpcclient/cli.go
+++ cli/rpcclient/cli.go
@@ -85,10 +85,16 @@ var ctrlDKeybind = prompt.OptionAddKeyBind(prompt.KeyBind{
 })
 
 // Run runs the CLI.
-func (c *CLI) Run() {
+func (c *CLI) Run(optArgs []string) {
 	color.Green("Welcome to the Ogen cli")
 	for {
-		t := prompt.Input("> ", completer, prompt.OptionCompletionWordSeparator(" "), ctrlCKeybind, ctrlDKeybind)
+		var t string
+		if len(optArgs) == 0 {
+			t = prompt.Input("> ", completer, prompt.OptionCompletionWordSeparator(" "), ctrlCKeybind, ctrlDKeybind)
+		} else {
+			t = strings.Join(optArgs, " ")
+			optArgs[0] = "exit"
+		}
 
 		args := strings.Split(t, " ")
 		if len(args) == 0 {
@@ -206,6 +212,11 @@ func (c *CLI) Run() {
 			out, err = c.rpcClient.startValidator(args[1:])
 		case "exitvalidator":
 			out, err = c.rpcClient.exitValidator(args[1:])
+
+		// Misc methods
+		case "exit":
+			return
+
 		default:
 			err = fmt.Errorf("Unknown command: %s", args[0])
 		}
@@ -227,5 +238,5 @@ func newCli(rpcClient *RPCClient) *CLI {
 func Run(cmd *cobra.Command, args []string) {
 	rpcClient := NewRPCClient("127.0.0.1:24127", viper.GetString("datadir"))
 	cli := newCli(rpcClient)
-	cli.Run()
+	cli.Run(args)
 }
