338159f4a08509336bd1e5da3f9cbe51ffcbebfb
Enrique Berrueta <eabz@polispay.org>
2020-08-26 10:52:31 -0500

    Remove CC tools on compiling scripts

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -3,14 +3,10 @@
 export OGEN_VERSION=0.0.1
 export FOLDER_NAME=ogen-$OGEN_VERSION
 
-echo $OGEN_VERSION
-echo $FOLDER_NAME
-
-
 echo "Building linux_amd64"
-CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/ogen/ogen.go
-CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
-CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/migration/migration.go
+GOOS=linux GOARCH=amd64 go build cmd/ogen/ogen.go
+GOOS=linux GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
+GOOS=linux GOARCH=amd64 go build cmd/migration/migration.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -20,9 +16,9 @@ tar -czvf ogen-$OGEN_VERSION-linux-amd64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building linux_arm64"
-CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/ogen/ogen.go
-CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/ogen-cli/ogen-cli.go
-CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/migration/migration.go
+GOOS=linux GOARCH=arm64 go build cmd/ogen/ogen.go
+GOOS=linux GOARCH=arm64 go build cmd/ogen-cli/ogen-cli.go
+GOOS=linux GOARCH=arm64 go build cmd/migration/migration.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -32,9 +28,9 @@ tar -czvf ogen-$OGEN_VERSION-linux-arm64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building linux_arm"
-CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ CGO_ENABLED=1 GOOS=linux GOARM=7 GOARCH=arm go build cmd/ogen/ogen.go
-CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ CGO_ENABLED=1 GOOS=linux GOARM=7 GOARCH=arm go build cmd/ogen-cli/ogen-cli.go
-CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ CGO_ENABLED=1 GOOS=linux GOARM=7 GOARCH=arm go build cmd/migration/migration.go
+GOOS=linux GOARM=7 GOARCH=arm go build cmd/ogen/ogen.go
+GOOS=linux GOARM=7 GOARCH=arm go build cmd/ogen-cli/ogen-cli.go
+GOOS=linux GOARM=7 GOARCH=arm go build cmd/migration/migration.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -44,9 +40,9 @@ tar -czvf ogen-$OGEN_VERSION-linux-arm.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building darwin_amd64"
-CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/ogen/ogen.go
-CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
-CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/migration/migration.go
+GOOS=darwin GOARCH=amd64 go build cmd/ogen/ogen.go
+GOOS=darwin GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
+GOOS=darwin GOARCH=amd64 go build cmd/migration/migration.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -56,9 +52,9 @@ tar -czvf ogen-$OGEN_VERSION-osx-amd64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building windows_amd64"
-CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen/ogen.go
-CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen-cli/ogen-cli.go
-CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/migration/migration.go
+GOOS=windows GOARCH=amd64 go build cmd/ogen/ogen.go
+GOOS=windows GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
+GOOS=windows GOARCH=amd64 go build cmd/migration/migration.go
 
 mkdir $FOLDER_NAME
 mv ogen.exe ./$FOLDER_NAME
