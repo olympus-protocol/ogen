9a93a0d0b756510721e54df9cc95697de22402e9
Aether-eum <aether@polispay.org>
2020-09-07 21:36:52 -0400

    build scripts for ogend

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -7,11 +7,15 @@ echo "Building linux_amd64"
 CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/ogen/ogen.go
 CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
 CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/migration/migration.go
+CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/ogen-d/ogen-d.go
+
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
 mv ogen-cli ./$FOLDER_NAME
 mv migration ./$FOLDER_NAME
+mv ogen-d ./$FOLDER_NAME
+
 tar -czvf ogen-$OGEN_VERSION-linux-amd64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
@@ -19,11 +23,13 @@ echo "Building linux_arm64"
 CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/ogen/ogen.go
 CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/ogen-cli/ogen-cli.go
 CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/migration/migration.go
+CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build cmd/ogen-d/ogen-d.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
 mv ogen-cli ./$FOLDER_NAME
 mv migration ./$FOLDER_NAME
+mv ogen-d ./$FOLDER_NAME
 tar -czvf ogen-$OGEN_VERSION-linux-arm64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
@@ -31,11 +37,14 @@ echo "Building darwin_amd64"
 CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/ogen/ogen.go
 CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/ogen-cli/ogen-cli.go
 CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/migration/migration.go
+CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build cmd/ogen-d/ogen-d.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
 mv ogen-cli ./$FOLDER_NAME
 mv migration ./$FOLDER_NAME
+mv ogen-d ./$FOLDER_NAME
+
 tar -czvf ogen-$OGEN_VERSION-osx-amd64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
@@ -43,10 +52,13 @@ echo "Building windows_amd64"
 CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen/ogen.go
 CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen-cli/ogen-cli.go
 CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/migration/migration.go
+CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen-d/ogen-d.go
 
 mkdir $FOLDER_NAME
 mv ogen.exe ./$FOLDER_NAME
 mv ogen-cli.exe ./$FOLDER_NAME
 mv migration.exe ./$FOLDER_NAME
+mv ogen-d.exe ./$FOLDER_NAME
+
 zip -r ogen-$OGEN_VERSION-windows-amd64.zip ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,4 @@
 go build cmd/ogen/ogen.go
 go build cmd/ogen-cli/ogen-cli.go
 go build cmd/migration/migration.go
+go build cmd/ogen-d/ogen-d.go
