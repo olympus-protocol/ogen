15b26d96a1293fe8354a679a2c1588caad7ca72d
Enrique Berrueta <eabz@polispay.org>
2020-10-02 08:42:09 -0500

    Fix var handling

--- cmd/ogen/commands/blockchain.go
+++ cmd/ogen/commands/blockchain.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"github.com/olympus-protocol/ogen/cmd/ogen/config"
 	"github.com/olympus-protocol/ogen/cmd/ogen/initialization"
 	"github.com/olympus-protocol/ogen/internal/blockdb"
@@ -14,96 +15,108 @@ import (
 	"time"
 )
 
-// loadOgen is the main function to run ogen.
-func loadOgen() error {
-
-	db, err := blockdb.NewBadgerDB()
-	if err != nil {
-		return err
-	}
-
-	s, err := server.NewServer(db)
-	if err != nil {
-		return err
-	}
-
-	go s.Start()
-
-	<-config.GlobalParams.Context.Done()
-
-	db.Close()
-	err = s.Stop()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 var (
-	rootCmd = &cobra.Command{
-		Use:   "ogen",
-		Short: "Ogen is a Go Olympus implementation",
-		Long: `A Golang implementation of the Olympus protocol.
-Next generation blockchain secured by CASPER.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			log := config.GlobalParams.Logger
+	DataPath string
+	NetName  string
+	Port     string
+	Debug    bool
+	LogFile  bool
 
-			log.Infof("Starting Ogen v%v", params.Version)
-			log.Trace("Loading log on debug mode")
-
-			config.InterruptListener()
-
-			err := loadOgen()
-			if err != nil {
-				log.Fatal(err)
-			}
-		},
-	}
+	RPCPort      string
+	RPCWallet    bool
+	RPCProxy     bool
+	RPCProxyPort string
+	RPCPRoxyAddr string
 )
-
-// Execute executes the root command.
-func Execute() error {
-	return rootCmd.Execute()
-}
 
 func init() {
 	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&DataPath, "datadir", "", "Directory to store the chain data.")
 
-	rootCmd.PersistentFlags().StringVar(&config.DataPath, "datadir", "", "Directory to store the chain data.")
-	rootCmd.PersistentFlags().Bool("debug", false, "Displays debug information.")
-	rootCmd.PersistentFlags().Bool("log_file", false, "Display log information to file.")
+	rootCmd.Flags().StringVar(&NetName, "network", "testnet", "String of the network to connect.")
+	rootCmd.Flags().StringVar(&Port, "port", "24126", "Default port for p2p connections listener.")
 
-	rootCmd.Flags().String("network", "testnet", "String of the network to connect.")
-	rootCmd.Flags().String("port", "24126", "Default port for p2p connections listener.")
+	rootCmd.Flags().BoolVar(&RPCProxy, "rpc_proxy", false, "Enable http proxy for RPC server.")
+	rootCmd.Flags().StringVar(&RPCProxyPort, "rpc_proxy_port", "8080", "Port for the http proxy.")
+	rootCmd.Flags().StringVar(&RPCPort, "rpc_port", "24127", "RPC server port.")
+	rootCmd.Flags().StringVar(&RPCPRoxyAddr, "rpc_proxy_addr", "localhost", "RPC proxy address to serve the http server.")
+	rootCmd.Flags().BoolVar(&RPCWallet, "rpc_wallet", false, "Enable wallet access through RPC.")
 
-	rootCmd.Flags().Bool("rpc_proxy", false, "Enable http proxy for RPC server.")
-	rootCmd.Flags().String("rpc_proxy_port", "8080", "Port for the http proxy.")
-	rootCmd.Flags().String("rpc_port", "24127", "RPC server port.")
-	rootCmd.Flags().String("rpc_proxy_addr", "localhost", "RPC proxy address to serve the http server.")
-	rootCmd.Flags().Bool("rpc_wallet", false, "Enable wallet access through RPC.")
+	rootCmd.PersistentFlags().BoolVar(&Debug, "debug", false, "Displays debug information.")
+	rootCmd.PersistentFlags().BoolVar(&LogFile, "logfile", false, "Display log information to file.")
 
 	err := viper.BindPFlags(rootCmd.PersistentFlags())
 	if err != nil {
 		panic(err)
 	}
 
+	err = viper.BindPFlags(rootCmd.Flags())
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initConfig() {
+	if DataPath != "" {
+		// Use config file from the flag.
+		viper.AddConfigPath(DataPath)
+		viper.SetConfigName("config")
+		if _, err := os.Stat(DataPath); os.IsNotExist(err) {
+			err = os.MkdirAll(DataPath, 0744)
+			if err != nil {
+				panic(err)
+			}
+		}
+	} else {
+		configDir, err := os.UserConfigDir()
+		if err != nil {
+			panic(err)
+		}
+
+		ogenDir := path.Join(configDir, "ogen")
+
+		if _, err := os.Stat(ogenDir); os.IsNotExist(err) {
+			err = os.MkdirAll(ogenDir, 0744)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		DataPath = ogenDir
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(ogenDir)
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+
+	err := viper.BindPFlags(rootCmd.Flags())
+	if err != nil {
+		panic(err)
+	}
+
 	config.GlobalFlags = &config.Flags{
-		DataPath:     config.DataPath,
-		NetworkName:  viper.GetString("network"),
-		Port:         viper.GetString("port"),
-		RPCProxy:     viper.GetBool("rpc_proxy"),
-		RPCProxyPort: viper.GetString("rpc_proxy_port"),
-		RPCProxyAddr: viper.GetString("rpc_proxy_addr"),
-		RPCPort:      viper.GetString("rpc_port"),
-		RPCWallet:    viper.GetBool("rpc_wallet"),
-		Debug:        viper.GetBool("debug"),
-		LogFile:      viper.GetBool("log_file"),
+		DataPath:     DataPath,
+		NetworkName:  NetName,
+		Port:         Port,
+		RPCProxy:     RPCProxy,
+		RPCProxyPort: RPCProxyPort,
+		RPCProxyAddr: RPCPRoxyAddr,
+		RPCPort:      RPCPort,
+		RPCWallet:    RPCWallet,
+		Debug:        Debug,
+		LogFile:      LogFile,
 	}
 
 	var log logger.Logger
 
 	if config.GlobalFlags.LogFile {
-		logFile, err := os.OpenFile(path.Join(config.GlobalFlags.DataPath, "logger.log"), os.O_CREATE|os.O_RDWR, 0755)
+		logFile, err := os.OpenFile(path.Join(DataPath, "logger.log"), os.O_CREATE|os.O_RDWR, 0755)
 		if err != nil {
 			panic(err)
 		}
@@ -158,37 +171,43 @@ func init() {
 	}
 }
 
-func initConfig() {
-	if config.DataPath != "" {
-		// Use config file from the flag.
-		viper.AddConfigPath(config.DataPath)
-		viper.SetConfigName("config")
-	} else {
-		configDir, err := os.UserConfigDir()
+var rootCmd = &cobra.Command{
+	Use:   "ogen",
+	Short: "Ogen is a Go Olympus implementation",
+	Long:  "A Golang implementation of the Olympus protocol. Next generation blockchain secured by CASPER.",
+	Run: func(cmd *cobra.Command, args []string) {
+		log := config.GlobalParams.Logger
+
+		log.Infof("Starting Ogen v%v", params.Version)
+		log.Trace("Loading log on debug mode")
+
+		config.InterruptListener()
+
+		db, err := blockdb.NewBadgerDB()
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
-		ogenDir := path.Join(configDir, "ogen")
-
-		if _, err := os.Stat(ogenDir); os.IsNotExist(err) {
-			err = os.MkdirAll(ogenDir, 0744)
-			if err != nil {
-				panic(err)
-			}
+		s, err := server.NewServer(db)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		config.DataPath = ogenDir
+		go s.Start()
 
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(ogenDir)
-		viper.SetConfigName("config")
-	}
+		<-config.GlobalParams.Context.Done()
 
-	viper.AutomaticEnv()
+		db.Close()
+		err = s.Stop()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Fatal(err)
+	},
+}
 
-	err := viper.BindPFlags(rootCmd.Flags())
-	if err != nil {
-		panic(err)
-	}
+func Execute() error {
+	fmt.Println(config.GlobalFlags)
+	fmt.Println(config.GlobalParams)
+	return rootCmd.Execute()
 }
