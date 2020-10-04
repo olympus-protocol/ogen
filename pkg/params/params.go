065664f8dbe090d469e3b5128bbbd4be3421d66f
Enrique Berrueta <eabz@polispay.org>
2020-10-04 12:24:45 -0500

    Remove relayers temporaly

--- pkg/params/params.go
+++ pkg/params/params.go
@@ -174,10 +174,8 @@ var TestNet = ChainParams{
 		0: "do_not_go_gentle_into_that_good_night",
 	},
 	Relayers: map[string]string{
-		"cronos-1": "/ip4/134.122.28.156/tcp/25000/p2p/12D3KooWDbvbYMzEvRi5CECVHawadaaZLich93GRhaCRRxQHFVcN",
-		"cronos-2": "/ip4/159.65.233.200/tcp/25000/p2p/12D3KooWBdwHS4aLQmfzhddfsT3aV9YA8Cy47VWx8ckoYkC8Vwb4",
-		"evolbyte": "/ip4/136.244.90.254/tcp/25000/p2p/12D3KooWNTqMjzeGDuyGDLm3kADGM6FWqN4fV2mem38Y1tEoxEdZ",
-		"stepollo": "/ip4/45.32.184.252/tcp/25000/p2p/12D3KooWStKhuQayJhKKqnA2HLqo2NrEP9dKhD7Ra7GM5TYUhCNW",
+		"cronos-1": "/ip4/134.122.28.156/tcp/25000/p2p/12D3KooW9vP2a7eKqGbKDWoAnhB9jc2ceBwHYRLXPmSu2TFXxjWt",
+		"cronos-2": "/ip4/159.65.233.200/tcp/25000/p2p/12D3KooWSrL9n292KSqww7MAzw9stNRrjuc25xeMFtXHBDnRAHAn",
 	},
 }
 
