9dc5775a425de2f28e666b6abac42c0bc4b469a1
Enrique Berrueta <eabz@polispay.org>
2020-10-04 09:43:48 -0500

    Remove profiler

--- cmd/ogen/ogen.go
+++ cmd/ogen/ogen.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"github.com/olympus-protocol/ogen/cmd/ogen/commands"
-	"net/http"
-	_ "net/http/pprof"
 )
 
 func main() {
-	go http.ListenAndServe(":1234", nil)
 	err := commands.Execute()
 	if err != nil {
 		panic(err)
