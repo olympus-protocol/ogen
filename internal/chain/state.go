945ce5e208c1ef2c8a8d91b8b23ee549b27bd33a
Enrique Berrueta <eabz@polispay.org>
2020-10-05 14:23:34 -0500

    Speed slots

--- internal/chain/state.go
+++ internal/chain/state.go
@@ -291,10 +291,12 @@ func (s *stateService) Add(block *primitives.Block) (state.State, []*primitives.
 func (s *stateService) RemoveBeforeSlot(slot uint64) {
 	s.stateMapLock.Lock()
 	defer s.stateMapLock.Unlock()
-	if slot <= 0 || slot < 20 {
+	fmt.Println(slot)
+	if slot < 20 {
 		return
 	}
 	slot -= 20
+	fmt.Println(slot)
 	statesCleaned := 0
 	for i := range s.stateMap {
 		row, found := s.index.Get(i)
