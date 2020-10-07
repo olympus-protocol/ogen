ce216f23f7b2ad0f2641fbba0062e2fb3c1a55cf
Enrique Berrueta <eabz@polispay.org>
2020-10-07 09:27:03 -0500

    Merge swagger

--- scripts/protoc-gen.sh
+++ scripts/protoc-gen.sh
@@ -8,7 +8,7 @@ curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/h
 echo "Generating protocol buffer definitions"
 protoc --proto_path=api/proto_def --go_out=plugins=grpc,paths=source_relative:api/proto api/proto_def/*.proto
 protoc --proto_path=api/proto_def --grpc-gateway_out=logtostderr=true,paths=source_relative:api/proto api/proto_def/*.proto
-protoc --proto_path=api/proto_def --swagger_out=logtostderr=true:api/swagger api/proto_def/*.proto
+protoc --proto_path=api/proto_def --swagger_out=allow_merge=true,merge_file_name=ogen,fqn_for_swagger_name=true,logtostderr=true:api/swagger api/proto_def/*.proto
 
 rm -rf api/proto_def/google
 
