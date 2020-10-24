4177d75f6c0f21ff350c85b7fd5ca8912d40ddf3
Enrique Berrueta <eabz@polispay.org>
2020-10-23 23:20:29 -0500

    include fixes and add slots and epochs to the db models

--- cmd/ogen/indexer/db/migrations/mysql/000001_initialize.up.sql
+++ cmd/ogen/indexer/db/migrations/mysql/000001_initialize.up.sql
@@ -1,161 +1,170 @@
-CREATE TABLE IF NOT EXISTS `blocks` (
-    `block_hash` binary(64) NOT NULL,
-    `block_signature` varbinary(192) NOT NULL,
-    `block_randao_signature` varbinary(192) NOT NULL,
-    `height` int NOT NULL,
-    PRIMARY KEY (`block_hash`),
-    UNIQUE KEY `block_hash_UNIQUE` (`block_hash`),
-    UNIQUE KEY `height_UNIQUE` (`height`)
+CREATE TABLE `blocks` (
+  `block_hash` varchar(255) PRIMARY KEY NOT NULL,
+  `block_signature` varchar(255) NOT NULL,
+  `block_randao_signature` varchar(255) NOT NULL,
+  `height` int NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `block_headers` (
-    `block_hash` binary(64) NOT NULL,
-    `version` INT NOT NULL,
-    `nonce` INT NOT NULL,
-    `tx_merkle_root` binary(64) NOT NULL,
-    `tx_multi_merkle_root` binary(64) NOT NULL,
-    `vote_merkle_root` binary(64) NOT NULL,
-    `deposit_merkle_root` binary(64) NOT NULL,
-    `exit_merkle_root` binary(64) NOT NULL,
-    `vote_slashing_merkle_root` binary(64) NOT NULL,
-    `randao_slashing_merkle_root` binary(64) NOT NULL,
-    `proposer_slashing_merkle_root` binary(64) NOT NULL,
-    `governance_votes_merkle_root` binary(64) NOT NULL,
-    `previous_block_hash` binary(64) NOT NULL,
-    `timestamp` INT NOT NULL,
-    `slot` INT NOT NULL,
-    `state_root` binary(64) NOT NULL,
-    `fee_address` binary(40) NOT NULL,
-    UNIQUE INDEX `block_hash_UNIQUE` (`block_hash` ASC) VISIBLE,
-    CONSTRAINT `block_hash`
-        FOREIGN KEY (`block_hash`)
-            REFERENCES `blocks` (`block_hash`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+CREATE TABLE `block_headers` (
+  `block_hash` varchar(255) NOT NULL,
+  `version` int NOT NULL,
+  `nonce` bigint NOT NULL,
+  `tx_merkle_root` varchar(255) NOT NULL,
+  `tx_multi_merkle_root` varchar(255) NOT NULL,
+  `vote_merkle_root` varchar(255) NOT NULL,
+  `deposit_merkle_root` varchar(255) NOT NULL,
+  `exit_merkle_root` varchar(255) NOT NULL,
+  `vote_slashing_merkle_root` varchar(255) NOT NULL,
+  `randao_slashing_merkle_root` varchar(255) NOT NULL,
+  `proposer_slashing_merkle_root` varchar(255) NOT NULL,
+  `governance_votes_merkle_root` varchar(255) NOT NULL,
+  `previous_block_hash` varchar(255) NOT NULL,
+  `timestamp` int NOT NULL,
+  `slot` int NOT NULL,
+  `state_root` varchar(255) NOT NULL,
+  `fee_address` varchar(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `votes` (
-    `block_hash` binary(64) NOT NULL,
-    `signature` binary(192) NOT NULL,
-    `participation_bitfield` varbinary(12516) NOT NULL,
-    `data_slot` INT NOT NULL,
-    `data_from_epoch` INT NOT NULL,
-    `data_from_hash` binary(64) NOT NULL,
-    `data_to_epoch` INT NOT NULL,
-    `data_to_hash` binary(64) NOT NULL,
-    `data_beacon_block_hash` binary(64) NOT NULL,
-    `data_nonce` INT NOT NULL,
-    `vote_hash` binary(64) NOT NULL,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE `votes` (
+  `block_hash` varchar(255) NOT NULL,
+  `signature` varchar(255) NOT NULL,
+  `participation_bitfield` varchar(12518) NOT NULL,
+  `data_slot` int NOT NULL,
+  `data_from_epoch` int NOT NULL,
+  `data_from_hash` varchar(255) NOT NULL,
+  `data_to_epoch` int NOT NULL,
+  `data_to_hash` varchar(255) NOT NULL,
+  `data_beacon_block_hash` varchar(255) NOT NULL,
+  `data_nonce` bigint NOT NULL,
+  `vote_hash` varchar(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS `deposits` (
-    `block_hash` binary(64) NOT NULL,
-    `public_key` BINARY(96) NOT NULL,
-    `signature` binary(192) NOT NULL,
-    `data_public_key` BINARY(96) NOT NULL,
-    `data_proof_of_possession` binary(192) NOT NULL,
-    `data_withdrawal_address` binary(40) NOT NULL,
-    UNIQUE INDEX `data_public_key_UNIQUE` (`data_public_key` ASC) VISIBLE,
-    UNIQUE INDEX `data_proof_of_possession_UNIQUE` (`data_proof_of_possession` ASC) VISIBLE,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+
+CREATE TABLE `deposits` (
+  `block_hash` varchar(255) NOT NULL,
+  `public_key` varchar(255) NOT NULL,
+  `signature` varchar(255) NOT NULL,
+  `data_public_key` varchar(255) NOT NULL,
+  `data_proof_of_possession` varchar(255) NOT NULL,
+  `data_withdrawal_address` varchar(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS `accounts` (
-    `account` binary(40) NOT NULL,
-    `confirmed` INT DEFAULT 0,
-    `unconfirmed` INT DEFAULT 0,
-    `locked`INT DEFAULT 0,
-    `total_sent` INT DEFAULT 0,
-    `total_received`INT DEFAULT 0,
-    UNIQUE INDEX `account_UNIQUE` (`account` ASC) VISIBLE
+
+CREATE TABLE `accounts` (
+  `account` varchar(255) NOT NULL,
+  `confirmed` bigint DEFAULT 0,
+  `unconfirmed` bigint DEFAULT 0,
+  `locked` bigint DEFAULT 0,
+  `total_sent` bigint DEFAULT 0,
+  `total_received` bigint DEFAULT 0
 );
-CREATE TABLE IF NOT EXISTS `validators` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `public_key` BINARY(96) NOT NULL,
-    `exit` BOOLEAN NOT NULL,
-    `penalized` BOOLEAN NOT NULL,
-    PRIMARY KEY (`id`)
+
+CREATE TABLE `validators` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `public_key` varchar(255) NOT NULL,
+  `status` int NOT NULL,
+  `exit` boolean NOT NULL,
+  `penalized` boolean NOT NULL,
+  `balance` bigint DEFAULT 0
 );
-CREATE TABLE IF NOT EXISTS `exits` (
-    `block_hash` binary(64) NOT NULL,
-    `validator_public_key` binary(192) NOT NULL,
-    `withdrawal_public_key` binary(40) NOT NULL,
-    `signature` binary(192) NOT NULL,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+
+CREATE TABLE `exits` (
+  `block_hash` varchar(255) NOT NULL,
+  `validator_public_key` varchar(255) NOT NULL,
+  `withdrawal_public_key` varchar(255) NOT NULL,
+  `signature` varchar(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS `tx_single` (
-    `hash` binary(64) NOT NULL,
-    `block_hash` binary(64) NOT NULL,
-    `tx_type` INT NOT NULL,
-    `to_addr` BINARY(40) NOT NULL,
-    `from_public_key` BINARY(96) NOT NULL,
-    `amount` INT NOT NULL,
-    `nonce` INT NOT NULL,
-    `fee` INT NOT NULL,
-    `signature` binary(192) NOT NULL,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+
+CREATE TABLE `tx_single` (
+  `hash` varchar(255) NOT NULL,
+  `block_hash` varchar(255) NOT NULL,
+  `tx_type` int NOT NULL,
+  `to_addr` varchar(255) NOT NULL,
+  `from_public_key` varchar(255) NOT NULL,
+  `from_public_key_hash` varchar(255) NOT NULL,
+  `amount` bigint NOT NULL,
+  `nonce` int NOT NULL,
+  `fee` bigint NOT NULL,
+  `signature` varchar(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS `vote_slashing` (
-    `block_hash` binary(64) NOT NULL,
-    `vote_1` binary(64) NOT NULL,
-    `vote_2` binary(64) NOT NULL,
-    PRIMARY KEY (`block_hash`, `vote_1`, `vote_2`),
-    UNIQUE INDEX `block_hash_UNIQUE` (`block_hash` ASC) VISIBLE,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+
+CREATE TABLE `vote_slashing` (
+  `block_hash` varchar(255) NOT NULL,
+  `vote_1` varchar(255) NOT NULL,
+  `vote_2` varchar(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS `randao_slashing` (
-    `block_hash` binary(64) NOT NULL,
-    `randao_reveal` binary(192) NOT NULL,
-    `slot` INT NOT NULL,
-    `validator_public_key` BINARY(96) NOT NULL,
-    PRIMARY KEY (`block_hash`),
-    UNIQUE INDEX `block_hash_UNIQUE` (`block_hash` ASC) VISIBLE,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (`validator_public_key`)
-        REFERENCES `deposits` (`data_public_key`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+
+CREATE TABLE `randao_slashing` (
+  `block_hash` varchar(255) PRIMARY KEY NOT NULL,
+  `randao_reveal` varchar(255) NOT NULL,
+  `slot` int NOT NULL,
+  `validator_public_key` varchar(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS `proposer_slashing` (
-    `block_hash` binary(64) NOT NULL,
-    `blockheader_1` binary(64) NOT NULL,
-    `blockheader_2` binary(64) NOT NULL,
-    `signature_1` binary(192) NOT NULL,
-    `signature_2` binary(192) NOT NULL,
-    `validator_public_key` BINARY(96) NOT NULL,
-    PRIMARY KEY (`block_hash`),
-    UNIQUE INDEX `block_hash_UNIQUE` (`block_hash` ASC) VISIBLE,
-    FOREIGN KEY (`block_hash`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (`blockheader_1`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (`blockheader_2`)
-        REFERENCES `blocks` (`block_hash`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (`validator_public_key`)
-        REFERENCES `deposits` (`data_public_key`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+
+CREATE TABLE `proposer_slashing` (
+  `block_hash` varchar(255) PRIMARY KEY NOT NULL,
+  `blockheader_1` varchar(255) NOT NULL,
+  `blockheader_2` varchar(255) NOT NULL,
+  `signature_1` varchar(255) NOT NULL,
+  `signature_2` varchar(255) NOT NULL,
+  `validator_public_key` varchar(255) NOT NULL
 );
+
+CREATE TABLE `slots` (
+  `slot` int PRIMARY KEY NOT NULL,
+  `block_hash` varchar(255) NOT NULL,
+  `committee` varchar(12518) NOT NULL,
+  `proposer_index` int NOT NULL,
+  `proposed` boolean NOT NULL,
+  `participation_percentage` int NOT NULL
+);
+
+CREATE TABLE `epochs` (
+  `epoch` int PRIMARY KEY NOT NULL,
+  `slot_1` int NOT NULL,
+  `slot_2` int NOT NULL,
+  `slot_3` int NOT NULL,
+  `slot_4` int NOT NULL,
+  `slot_5` int NOT NULL,
+  `participation_percentage` int NOT NULL
+);
+
+ALTER TABLE `blocks` ADD FOREIGN KEY (`block_hash`) REFERENCES `block_headers` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `votes` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `deposits` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `exits` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `vote_slashing` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `randao_slashing` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `proposer_slashing` (`block_hash`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `proposer_slashing` (`blockheader_1`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `proposer_slashing` (`blockheader_2`);
+
+ALTER TABLE `block_headers` ADD FOREIGN KEY (`block_hash`) REFERENCES `tx_single` (`block_hash`);
+
+ALTER TABLE `deposits` ADD FOREIGN KEY (`data_public_key`) REFERENCES `validators` (`public_key`);
+
+ALTER TABLE `deposits` ADD FOREIGN KEY (`data_public_key`) REFERENCES `proposer_slashing` (`validator_public_key`);
+
+ALTER TABLE `deposits` ADD FOREIGN KEY (`public_key`) REFERENCES `exits` (`validator_public_key`);
+
+ALTER TABLE `deposits` ADD FOREIGN KEY (`public_key`) REFERENCES `randao_slashing` (`validator_public_key`);
+
+ALTER TABLE `accounts` ADD FOREIGN KEY (`account`) REFERENCES `tx_single` (`to_addr`);
+
+ALTER TABLE `accounts` ADD FOREIGN KEY (`account`) REFERENCES `tx_single` (`from_public_key_hash`);
+
+ALTER TABLE `slots` ADD FOREIGN KEY (`slot`) REFERENCES `epochs` (`slot_1`);
+
+ALTER TABLE `slots` ADD FOREIGN KEY (`slot`) REFERENCES `epochs` (`slot_2`);
+
+ALTER TABLE `slots` ADD FOREIGN KEY (`slot`) REFERENCES `epochs` (`slot_3`);
+
+ALTER TABLE `slots` ADD FOREIGN KEY (`slot`) REFERENCES `epochs` (`slot_4`);
+
+ALTER TABLE `slots` ADD FOREIGN KEY (`slot`) REFERENCES `epochs` (`slot_5`);
--- cmd/ogen/indexer/db/migrations/postgres/000001_initialize.up.sql
+++ cmd/ogen/indexer/db/migrations/postgres/000001_initialize.up.sql
@@ -1,166 +1,170 @@
-CREATE TABLE IF NOT EXISTS blocks (
-    block_hash text NOT NULL,
-    block_signature text NOT NULL,
-    block_randao_signature text NOT NULL,
-    height integer NOT NULL,
-    UNIQUE(block_hash),
-    PRIMARY KEY (block_hash)
+CREATE TABLE "blocks" (
+    "block_hash" varchar PRIMARY KEY NOT NULL,
+    "block_signature" varchar NOT NULL,
+    "block_randao_signature" varchar NOT NULL,
+    "height" int NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS block_headers (
-    block_hash text NOT NULL,
-    version BIGINT NOT NULL,
-    nonce BIGINT NOT NULL,
-    tx_merkle_root text NOT NULL,
-    tx_multi_merkle_root text NOT NULL,
-    vote_merkle_root text NOT NULL,
-    deposit_merkle_root text NOT NULL,
-    exit_merkle_root text NOT NULL,
-    vote_slashing_merkle_root text NOT NULL,
-    randao_slashing_merkle_root text NOT NULL,
-    proposer_slashing_merkle_root text NOT NULL,
-    governance_votes_merkle_root text NOT NULL,
-    previous_block_hash text NOT NULL,
-    timestamp BIGINT NOT NULL,
-    slot BIGINT NOT NULL,
-    state_root text NOT NULL,
-    fee_address text NOT NULL,
-    CONSTRAINT block_hash
-        FOREIGN KEY (block_hash)
-            REFERENCES blocks (block_hash)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+CREATE TABLE "block_headers" (
+    "block_hash" varchar NOT NULL,
+    "version" int NOT NULL,
+    "nonce" bigint NOT NULL,
+    "tx_merkle_root" varchar NOT NULL,
+    "tx_multi_merkle_root" varchar NOT NULL,
+    "vote_merkle_root" varchar NOT NULL,
+    "deposit_merkle_root" varchar NOT NULL,
+    "exit_merkle_root" varchar NOT NULL,
+    "vote_slashing_merkle_root" varchar NOT NULL,
+    "randao_slashing_merkle_root" varchar NOT NULL,
+    "proposer_slashing_merkle_root" varchar NOT NULL,
+    "governance_votes_merkle_root" varchar NOT NULL,
+    "previous_block_hash" varchar NOT NULL,
+    "timestamp" int NOT NULL,
+    "slot" int NOT NULL,
+    "state_root" varchar NOT NULL,
+    "fee_address" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS votes (
-    block_hash text NOT NULL,
-    signature text NOT NULL,
-    participation_bitfield text NOT NULL,
-    data_slot BIGINT NOT NULL,
-    data_from_epoch BIGINT NOT NULL,
-    data_from_hash text NOT NULL,
-    data_to_epoch BIGINT NOT NULL,
-    data_to_hash text NOT NULL,
-    data_beacon_block_hash text NOT NULL,
-    data_nonce BIGINT NOT NULL,
-    vote_hash text NOT NULL,
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "votes" (
+    "block_hash" varchar NOT NULL,
+    "signature" varchar NOT NULL,
+    "participation_bitfield" varchar(12518) NOT NULL,
+    "data_slot" int NOT NULL,
+    "data_from_epoch" int NOT NULL,
+    "data_from_hash" varchar NOT NULL,
+    "data_to_epoch" int NOT NULL,
+    "data_to_hash" varchar NOT NULL,
+    "data_beacon_block_hash" varchar NOT NULL,
+    "data_nonce" bigint NOT NULL,
+    "vote_hash" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS deposits (
-    block_hash text NOT NULL,
-    public_key BYTEA NOT NULL,
-    signature text NOT NULL,
-    data_public_key BYTEA NOT NULL,
-    data_proof_of_possession text NOT NULL,
-    data_withdrawal_address text NOT NULL,
-    UNIQUE(data_public_key),
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "deposits" (
+    "block_hash" varchar NOT NULL,
+    "public_key" varchar NOT NULL,
+    "signature" varchar NOT NULL,
+    "data_public_key" varchar NOT NULL,
+    "data_proof_of_possession" varchar NOT NULL,
+    "data_withdrawal_address" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS accounts (
-    account text NOT NULL,
-    confirmed BIGINT DEFAULT 0,
-    unconfirmed BIGINT DEFAULT 0,
-    locked BIGINT DEFAULT 0,
-    total_sent BIGINT DEFAULT 0,
-    total_received BIGINT DEFAULT 0
+CREATE TABLE "accounts" (
+    "account" varchar NOT NULL,
+    "confirmed" bigint DEFAULT 0,
+    "unconfirmed" bigint DEFAULT 0,
+    "locked" bigint DEFAULT 0,
+    "total_sent" bigint DEFAULT 0,
+    "total_received" bigint DEFAULT 0
 );
 
-CREATE SEQUENCE validators_seq;
-
-CREATE TABLE IF NOT EXISTS validators (
-    id BIGINT NOT NULL DEFAULT NEXTVAL ('validators_seq'),
-    public_key BYTEA NOT NULL,
-    exit BOOLEAN NOT NULL,
-    penalized BOOLEAN NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE "validators" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "public_key" varchar NOT NULL,
+    "status" int NOT NULL,
+    "exit" boolean NOT NULL,
+    "penalized" boolean NOT NULL,
+    "balance" bigint DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS exits (
-    block_hash text NOT NULL,
-    validator_public_key text NOT NULL,
-    withdrawal_public_key text NOT NULL,
-    signature text NOT NULL,
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "exits" (
+    "block_hash" varchar NOT NULL,
+    "validator_public_key" varchar NOT NULL,
+    "withdrawal_public_key" varchar NOT NULL,
+    "signature" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS tx_single (
-    hash text NOT NULL,
-    block_hash text NOT NULL,
-    tx_type BIGINT NOT NULL,
-    to_addr BYTEA NOT NULL,
-    from_public_key BYTEA NOT NULL,
-    amount BIGINT NOT NULL,
-    nonce BIGINT NOT NULL,
-    fee BIGINT NOT NULL,
-    signature text NOT NULL,
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "tx_single" (
+    "hash" varchar NOT NULL,
+    "block_hash" varchar NOT NULL,
+    "tx_type" int NOT NULL,
+    "to_addr" varchar NOT NULL,
+    "from_public_key" varchar NOT NULL,
+    "from_public_key_hash" varchar NOT NULL,
+    "amount" bigint NOT NULL,
+    "nonce" int NOT NULL,
+    "fee" bigint NOT NULL,
+    "signature" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS vote_slashing (
-    block_hash text NOT NULL,
-    vote_1 text NOT NULL,
-    vote_2 text NOT NULL,
-    PRIMARY KEY (block_hash, vote_1, vote_2),
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "vote_slashing" (
+    "block_hash" varchar NOT NULL,
+    "vote_1" varchar NOT NULL,
+    "vote_2" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS randao_slashing (
-    block_hash text NOT NULL,
-    randao_reveal text NOT NULL,
-    slot BIGINT NOT NULL,
-    validator_public_key BYTEA NOT NULL,
-    PRIMARY KEY (block_hash),
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (validator_public_key)
-        REFERENCES deposits (data_public_key)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "randao_slashing" (
+    "block_hash" varchar PRIMARY KEY NOT NULL,
+    "randao_reveal" varchar NOT NULL,
+    "slot" int NOT NULL,
+    "validator_public_key" varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS proposer_slashing (
-    block_hash text NOT NULL,
-    blockheader_1 text NOT NULL,
-    blockheader_2 text NOT NULL,
-    signature_1 text NOT NULL,
-    signature_2 text NOT NULL,
-    validator_public_key BYTEA NOT NULL,
-    PRIMARY KEY (block_hash),
-    FOREIGN KEY (block_hash)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (blockheader_1)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (blockheader_2)
-        REFERENCES blocks (block_hash)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    FOREIGN KEY (validator_public_key)
-        REFERENCES deposits (data_public_key)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+CREATE TABLE "proposer_slashing" (
+    "block_hash" varchar PRIMARY KEY NOT NULL,
+    "blockheader_1" varchar NOT NULL,
+    "blockheader_2" varchar NOT NULL,
+    "signature_1" varchar NOT NULL,
+    "signature_2" varchar NOT NULL,
+    "validator_public_key" varchar NOT NULL
 );
 
+CREATE TABLE "slots" (
+    "slot" int PRIMARY KEY NOT NULL,
+    "block_hash" varchar NOT NULL,
+    "committee" varchar(12518) NOT NULL,
+    "proposer_index" int NOT NULL,
+    "proposed" boolean NOT NULL,
+    "participation_percentage" int NOT NULL
+);
 
+CREATE TABLE "epochs" (
+    "epoch" int PRIMARY KEY NOT NULL,
+    "slot_1" int NOT NULL,
+    "slot_2" int NOT NULL,
+    "slot_3" int NOT NULL,
+    "slot_4" int NOT NULL,
+    "slot_5" int NOT NULL,
+    "participation_percentage" int NOT NULL
+);
+
+ALTER TABLE "blocks" ADD FOREIGN KEY ("block_hash") REFERENCES "block_headers" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "votes" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "deposits" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "exits" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "vote_slashing" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "randao_slashing" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "proposer_slashing" ("block_hash");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "proposer_slashing" ("blockheader_1");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "proposer_slashing" ("blockheader_2");
+
+ALTER TABLE "block_headers" ADD FOREIGN KEY ("block_hash") REFERENCES "tx_single" ("block_hash");
+
+ALTER TABLE "deposits" ADD FOREIGN KEY ("data_public_key") REFERENCES "validators" ("public_key");
+
+ALTER TABLE "deposits" ADD FOREIGN KEY ("data_public_key") REFERENCES "proposer_slashing" ("validator_public_key");
+
+ALTER TABLE "deposits" ADD FOREIGN KEY ("public_key") REFERENCES "exits" ("validator_public_key");
+
+ALTER TABLE "deposits" ADD FOREIGN KEY ("public_key") REFERENCES "randao_slashing" ("validator_public_key");
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("account") REFERENCES "tx_single" ("to_addr");
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("account") REFERENCES "tx_single" ("from_public_key_hash");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("slot") REFERENCES "epochs" ("slot_1");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("slot") REFERENCES "epochs" ("slot_2");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("slot") REFERENCES "epochs" ("slot_3");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("slot") REFERENCES "epochs" ("slot_4");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("slot") REFERENCES "epochs" ("slot_5");
