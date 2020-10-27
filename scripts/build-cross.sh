6b74bfba2d7c8b63f088bb61204734cd6e3befb0
Enrique Berrueta <eabz@polispay.org>
2020-10-27 10:48:42 -0600

    Fix build scripts

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-go run github.com/markbates/pkger/cmd/pkger -o cmd/ogen
-
 export OGEN_VERSION=0.1.0-alpha.6
 export FOLDER_NAME=ogen-$OGEN_VERSION
 
+go get -u github.com/gobuffalo/packr/packr
+
 echo "Building linux_amd64"
-CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build cmd/ogen/ogen.go
+CC=x86_64-linux-gnu-gcc CXX=x86_64-linux-gnu-g++  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 packr build cmd/ogen/ogen.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -15,7 +15,7 @@ tar -czvf ogen-$OGEN_VERSION-linux-amd64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building linux_arm64"
-CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go run github.com/gobuffalo/packr/packr build cmd/ogen/ogen.go
+CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CGO_ENABLED=1 GOOS=linux GOARCH=arm64 packr build cmd/ogen/ogen.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -24,7 +24,7 @@ tar -czvf ogen-$OGEN_VERSION-linux-arm64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building darwin_amd64"
-CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go run github.com/gobuffalo/packr/packr build cmd/ogen/ogen.go
+CXX=x86_64-apple-darwin19-clang++ CC=x86_64-apple-darwin19-clang CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 packr build cmd/ogen/ogen.go
 
 mkdir $FOLDER_NAME
 mv ogen ./$FOLDER_NAME
@@ -33,7 +33,7 @@ tar -czvf ogen-$OGEN_VERSION-osx-amd64.tar.gz ./$FOLDER_NAME
 rm -r ./$FOLDER_NAME
 
 echo "Building windows_amd64"
-CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go run github.com/gobuffalo/packr/packr build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen/ogen.go
+CXX=x86_64-w64-mingw32-c++ CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 packr build -tags netgo -ldflags '-extldflags "-static -static-libstdc++"' -a cmd/ogen/ogen.go
 
 mkdir $FOLDER_NAME
 mv ogen.exe ./$FOLDER_NAME
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go run github.com/gobuffalo/packr/packr build cmd/ogen/ogen.go
-
+go get -u github.com/gobuffalo/packr/packr
+packr build cmd/ogen/ogen.go
