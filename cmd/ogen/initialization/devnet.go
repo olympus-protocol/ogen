9e72eb3d31c615cf196384f2595681424ff06045
Enrique Berrueta <eabz@polispay.org>
2020-12-20 19:58:13 -0600

    Devnet initialization params

---
+++ cmd/ogen/initialization/devnet.go
@@ -0,0 +1,41 @@
+package initialization
+
+var DevNet = NetworkInitialParams{
+	Validators: []Validators{
+		{
+			PublicKey:  "a45de5753f23a450a1e796f259cb4b21371e9c81dd83ae27512216220eb8d7579b6f149dced7da08b40139618be67822",
+			PrivateKey: "3d18aa3e5b51160cf214fcc397b57158c04c07de0f4b091c879595ac6d51f564",
+		},
+		{
+			PublicKey:  "b0b80297da236f496616a440818792628ccbf4c10a231db08f6b67a77bc54e999bb20dc156c5e86f3c1268839ff95802",
+			PrivateKey: "051c3787e5f08c0c36fd2408c64a0ad8e850a0b63a638ccdee19f30f1e15bfe3",
+		},
+		{
+			PublicKey:  "b989a61244df36d7a9396b34f192187a47c2f33f62355a53c5cdbf5dd65f9632df303c1569d5ff4431a1972971bba2ed",
+			PrivateKey: "36b4ad4b17a9ed5fc568cafb572ef2f14e3b56a2ef11a5dd2038ec9a713db026",
+		},
+		{
+			PublicKey:  "b6432b91b77c3add266c011443571339004debef4599a01119b55120896c9ae7546ab972f89ca711deb214e118b12aa2",
+			PrivateKey: "29babb5e3ab443ce249bff296f22eeb5a58784d799996a541b357f24ca0ff4e6",
+		},
+		{
+			PublicKey:  "93e0679c864bb48ec258d9f20717307ca7fcf586a59950e6a42b8e6c5e9da83afa01e532b7c2da868e17956feab1529b",
+			PrivateKey: "36b691165679f9713f576f57757125bd19d89da0b0a458645be9b1e57c740c23",
+		},
+		{
+			PublicKey:  "975544e54f0e12323b4ff6457a20b53f5957307e473e22cfa2e5323a23977078120bdd9577d09deca095c211f076ccd5",
+			PrivateKey: "0813d2ee2c9c426ed57b1fe2e35e30ee0c78b4eaa3e001db4ce84b8ca7fc638f",
+		},
+		{
+			PublicKey:  "ad90fb6fec846dd559f5fb440b04992dd9cf8c0fc1331871e0ac1158e8d6fabdcb426b4e7c325b8bc3df3f7fcb772226",
+			PrivateKey: "3f2202aa44718c80cee87cb323e092c6c086fa68af90a2d2322797a47a6f6010",
+		},
+		{
+			PublicKey:  "91750f11d71cd4d758ecbee5e466d82373558a0c8f188de84c35879731b2fff6c7e25c01eeb3400bc05bcda1b9d56a86",
+			PrivateKey: "3404cd54dbd1658f94bf00ca1ba88e391e8b66e1402d833c03e722a3385c258d",
+		},
+	},
+	PremineAddress:  "dlpub10negrvw98fw7zjty8qg0kyfr5c3wxafalrhn07",
+	PremineMnemonic: "snake angle spend afford problem harvest cherry clump clog dumb enact phone artist rail silver card extra impose stone wonder lecture apart kind floor",
+	GenesisTime:     0,
+}
