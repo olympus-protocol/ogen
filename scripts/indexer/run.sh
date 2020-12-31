acf27bbd9bc4e5425e844af03b9325185e658ef6
Enrique Berrueta <eabz@polispay.org>
2020-12-31 17:50:48 -0600

    Update

--- scripts/indexer/run.sh
+++ scripts/indexer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./ogen --network testnet --dashboard & ./ogen indexer testnet --dbconn="postgresql://localhost:5432/indexer"
+./ogen --network testnet --dashboard & ./ogen indexer testnet --dbconn="postgresql://postgres@localhost:5432/indexer"
