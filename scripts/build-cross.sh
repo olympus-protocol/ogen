546e2ffea1951f9213ea002ed7b04845bbc15bdf
Enrique Berrueta <eabz@polispay.org>
2021-01-08 10:03:20 -0600

    Update build-cross.sh

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -3,7 +3,9 @@
 export OGEN_VERSION=0.2.0-alpha.3
 export FOLDER_NAME=ogen-$OGEN_VERSION
 
-xgo --branch bls  -pkg cmd/ogen --targets=windows-6.0/amd64,darwin-10.10/amd64,linux/arm64,linux/amd64 -out ogen-$OGEN_VERSION github.com/olympus-protocol/ogen
+go get src.techknowlogick.com/xgo
+
+xgo --targets=windows-6.0/amd64,darwin-10.10/amd64,linux/arm64,linux/amd64 -out ogen-$OGEN_VERSION .
 
 mkdir ./$FOLDER_NAME
 mv ogen-$OGEN_VERSION-windows-6.0-amd64.exe ./$FOLDER_NAME/ogen.exe
@@ -29,3 +31,5 @@ gpg --detach-sign ogen-"$OGEN_VERSION"-linux-amd64.tar.gz
 gpg --detach-sign ogen-"$OGEN_VERSION"-linux-arm64.tar.gz
 gpg --detach-sign ogen-"$OGEN_VERSION"-osx-amd64.tar.gz
 gpg --detach-sign ogen-"$OGEN_VERSION"-windows-amd64.zip
+
+go mod tidy
