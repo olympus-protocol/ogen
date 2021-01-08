cd0cf7856dbdaf80bf5115e39fa52234160383db
Enrique Berrueta <eabz@polispay.org>
2021-01-08 10:22:33 -0600

    Fix tests

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -5,7 +5,7 @@ export FOLDER_NAME=ogen-$OGEN_VERSION
 
 go get src.techknowlogick.com/xgo
 
-xgo --targets=windows-6.0/amd64,darwin-10.10/amd64,linux/arm64,linux/amd64 -out ogen-$OGEN_VERSION .
+xgo -pkg cmd/ogen --targets=windows-6.0/amd64,darwin-10.15/amd64,linux/arm64,linux/amd64 -out ogen-$OGEN_VERSION .
 
 mkdir ./$FOLDER_NAME
 mv ogen-$OGEN_VERSION-windows-6.0-amd64.exe ./$FOLDER_NAME/ogen.exe
