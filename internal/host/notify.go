9ac3e78c8c464dbca5976e4dd91533de8f26f754
Enrique Berrueta <eabz@polispay.org>
2021-01-11 18:31:12 -0600

    WIP

--- internal/host/notify.go
+++ internal/host/notify.go
@@ -19,7 +19,7 @@ func (n notify) Connected(network network.Network, conn network.Conn) {
 }
 
 func (n notify) Disconnected(network network.Network, conn network.Conn) {
-	panic("implement me")
+
 }
 
 func (n notify) OpenedStream(network network.Network, stream network.Stream) {}
