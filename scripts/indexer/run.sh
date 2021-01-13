1117c3535325cd01c7121f1784fa53181114f57f
Enrique Berrueta <eabz@polispay.org>
2021-01-13 16:07:13 -0600

    Fix docker images rpc_proxy

--- scripts/indexer/run.sh
+++ scripts/indexer/run.sh
@@ -2,4 +2,4 @@
 
 service postgresql start
 
-./ogen --network testnet --dashboard --rpc_proxy & ./ogen indexer testnet --dbconn="postgresql://indexer:indexer@127.0.0.1/indexer"
+./ogen --network testnet --dashboard --rpc_proxy --rpc_proxy_addr 0.0.0.0 & ./ogen indexer testnet --dbconn="postgresql://indexer:indexer@127.0.0.1/indexer"
